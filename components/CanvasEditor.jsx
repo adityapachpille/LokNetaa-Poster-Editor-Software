980d67acb31650b23a8b9b46bc0421ec4ae82f24
adityapachpille <158599059+adityapachpille@users.noreply.github.com>
2025-11-23 02:07:19 +0530

    Update CanvasEditor.jsx

--- components/CanvasEditor.jsx
+++ components/CanvasEditor.jsx
@@ -128,7 +128,7 @@ export default function CanvasEditor() {
     <>
       {/* Top bar */}
       <div className="topBarWrapper">
-        <h2 className="title">📲🗳️ LokNetaa Poster Editor Softwareo 👥📸✨</h2>
+        <h2 className="title">📲🗳️ LokNetaa Poster Editor Software 👥📸✨</h2>
         <div className="topBar">
           <button className="button" onClick={() => fileRef.current?.click()}>
             📤 Import Photo
